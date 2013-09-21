4c39ebf42e5037f241f29ad2d5e1ae9aa800ccf7
Dave Juth <djuth@yahoo.com>
2013-09-21 16:12:22 -0400

    Stuck on deleting an article...

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  
+  
   def index
     @articles = Article.all
   end
@@ -6,4 +11,45 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    # fail # This will dump params in error view
+    
+    # Clean this up...
+    # @article = Article.new
+    # @article.title = params[:article][:title]
+    # @article.body = params[:article][:body]
+    #
+    # NOTE: this works but is unsafe:
+    #@article = Article.new(params[:article])
+    #
+    # Instead, see article_params method above, which declares accepted params,
+    # then pass that to the new article ctor.
+    @article = Article.new(article_params)
+    
+    @article.save
+    
+    # redirect_to article_path(@article)
+    redirect_to articles_path
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+    # @article = Article.find(params[:id])
+    # @article.destroy
+    
+    @article.destroy
+    
+    redirect_to articles_path
+  end
+  
 end
